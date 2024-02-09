9826f90a184885415fdc39b6972629022d2376a6
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-02-09 14:38:40 +0530

    udated services section

--- src/Componets/Services/services.jsx
+++ src/Componets/Services/services.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import "./services.css";
 import { NavLink } from "react-router-dom";
 
 export default function Services() {
   return (
     <div className="container  py-5" id="services">
-      <div class="line-long-area" data-aos="fade-up-right">
-        <div class="line-long"></div>
+      <div className="line-long-area" data-aos="fade-up-right">
+        <div className="line-long"></div>
       </div>
 
       <div className="row mt-3">
@@ -19,28 +18,21 @@ export default function Services() {
           <div className="colbox">
             <div className="boxicon">
               <span className="services-icon">
-                <i class="fa-solid fa-globe"></i>
+                <i className="fa-solid fa-globe"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href="http://127.0.0.1:5173/webDevelopment"
-              className="h4"
-            >
+            <a href="http://127.0.0.1:5173/webDevelopment" className="h4">
               Web Design &amp; Development
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  We craft creative and effective websites using the best design
-                  techniques in the business. Our design uses the finest
-                  elements that help the business to elevate its brand presence
-                  in the web space.
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                  We specialize in creating unique and impactful websites that incorporate cutting-edge design techniques. Our websites are crafted with the finest elements that help businesses establish a strong online presence and enhance their brand identity in the digital world.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  We specialize in creating unique and impactful websites that
+                  incorporate cutting-edge design techniques. Our websites are
+                  crafted with the finest elements that help businesses
+                  establish a strong online presence and enhance their brand
+                  identity in the digital world.
                 </p>
               </div>
             </div>
@@ -58,28 +50,21 @@ export default function Services() {
           <div className="colbox">
             <div className="boxicon">
               <span className="services-icon">
-                <i class="fa-solid fa-computer"></i>
+                <i className="fa-solid fa-computer"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href=""
-              className="h4"
-            >
+            <a href="" className="h4">
               Software Development
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  We offer custom software development services using all
-                  accepted and development technologies. Our software
-                  development service adapts seamlessly to your project
-                  requirements and business needs.
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                Our team specializes in providing tailored software development solutions using the latest technologies and industry best practices. We understand that every project has unique requirements and we strive to meet those needs while keeping in mind your business objectives.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  Our team specializes in providing tailored software
+                  development solutions using the latest technologies and
+                  industry best practices. We understand that every project has
+                  unique requirements and we strive to meet those needs while
+                  keeping in mind your business objectives.
                 </p>
               </div>
             </div>
@@ -97,28 +82,20 @@ export default function Services() {
           <div className="colbox">
             <div className="boxicon">
               <span className="services-icon">
-                <i class="fa-solid fa-mobile-screen"></i>
+                <i className="fa-solid fa-mobile-screen"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href="http://127.0.0.1:5173/appDevelopment"
-              className="h4"
-            >
+            <a href="http://127.0.0.1:5173/appDevelopment" className="h4">
               Mobile App Development
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  We build mobile applications that function perfectly with the
-                  whole spectrum of mobile devices and online platforms. Our
-                  experts cover an entire cycle of application design,
-                  integration, and management services.
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                Our team specializes in creating mobile apps that seamlessly work across various mobile devices and online platforms. We offer end-to-end solutions, from designing and integrating the app to managing its entire lifecycle.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  Our team specializes in creating mobile apps that seamlessly
+                  work across various mobile devices and online platforms. We
+                  offer end-to-end solutions, from designing and integrating the
+                  app to managing its entire lifecycle.
                 </p>
               </div>
             </div>
@@ -136,28 +113,21 @@ export default function Services() {
           <div className="colbox">
             <div className="boxicon">
               <span className="services-icon">
-                <i class="fa-solid fa-bullhorn"></i>
+                <i className="fa-solid fa-bullhorn"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href=""
-              className="h4"
-            >
+            <a href="" className="h4">
               Digital Marketing
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  Expand your products and services to reach the global level
-                  with the most cost-effective method. Our digital marketing
-                  service ensures the best rates of conversion of potential
-                  leads to customers.
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                Our company offers digital marketing services that can help you expand your business and reach a global audience in a cost-effective manner. Our team specializes in converting potential leads into loyal customers, ensuring a high return on investment for your marketing efforts.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  Our company offers digital marketing services that can help
+                  you expand your business and reach a global audience in a
+                  cost-effective manner. Our team specializes in converting
+                  potential leads into loyal customers, ensuring a high return
+                  on investment for your marketing efforts.
                 </p>
               </div>
             </div>
@@ -175,27 +145,20 @@ export default function Services() {
           <div className="colbox">
             <div className="boxicon">
               <span className="services-icon">
-                <i class="fa-solid fa-gear"></i>
+                <i className="fa-solid fa-gear"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href=""
-              className="h4"
-            >
+            <a href="" className="h4">
               ERP Development
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  The evolution of the ERP industry, which is driven by trends
-                  such as extensibility and integration, human-ERP interaction,
-                  SaaS adoption, vertical-specific standardization,
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                The development of ERP (Enterprise Resource Planning) involves designing, adapting and managing software systems that merge different business operations and procedures into a cohesive and centralized framework.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  The development of ERP (Enterprise Resource Planning) involves
+                  designing, adapting and managing software systems that merge
+                  different business operations and procedures into a cohesive
+                  and centralized framework.
                 </p>
               </div>
             </div>
@@ -212,27 +175,20 @@ export default function Services() {
             <div className="boxicon">
               <span className="services-icon">
                 {/* <i class="fa-solid fa-gear"></i> */}
-                <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+                <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
               </span>
             </div>
             {/* boxicon */}
-            <a
-              href=""
-              className="h4"
-            >
+            <a href="" className="h4">
               Search Engine Optimisation
             </a>
             <div className="boxcont">
               <div className="boxdesc">
-<<<<<<< HEAD
                 <p style={{ textAlign: "justify", marginTop: "5px" }}>
-                  Enhance your business on the world’s top search engines. We
-                  will help you improve the organic visibility of your business
-                  keywords and organic traffic to the website.
-=======
-                <p style={{textAlign: "justify", marginTop:"5px"}}>
-                Boost your business's online presence and increase your search engine visibility. We'll assist you in optimizing your website's content to improve your organic keyword ranking and drive more traffic to your site.
->>>>>>> 7db645e924a8c7321cba948fefecbe8ed2f3dd57
+                  Boost your business &apos; s online presence and increase your
+                  search engine visibility. We'll assist you in optimizing your
+                  website's content to improve your organic keyword ranking and
+                  drive more traffic to your site.
                 </p>
               </div>
             </div>
