5c792a48a05bde2e48b65afd161f4026e95a370e
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-06-06 06:59:22 +0530

    added logo slider using marque library react

--- src/Componets/Positbilities/Possibility.jsx
+++ src/Componets/Positbilities/Possibility.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import possibilityImage from '../../assets/representation-user-experience-interface-design.jpg';
 import './possibility.css';
-import Slider from '../Slider/Slider';
+
+
+const SliderLazy = lazy(()=>import('../Slider/Slider'))
 
 const Possibility = () => (
   <div>
     <div className="gpt3__possibility section__padding" id="possibility">
-    <div className="gpt3__possibility-image">
-      <img src={possibilityImage} alt="possibility" style={{borderRadius:"7px"}}/>
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" style={{ borderRadius: "7px" }} />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
-    <div className="gpt3__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
-      <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-      <h4>Request Early Access to Get Started</h4>
-    </div>
-  </div>
-  <Slider />
+    <SliderLazy />
   </div>
 );
 
--- src/Componets/Slider/Slider.jsx
+++ src/Componets/Slider/Slider.jsx
@@ -1,138 +1,129 @@
 import React from 'react'
-import './Slider.css'
 
-function Slider() {
+import './Slider.css'
+import Marquee from 'react-fast-marquee'
+
+function Slider2() {
+
   return (
     <div>
-         <div className="mt-5 text-center">
-                <div className="wpb_wrapper">
-                  <div className="ld-fancy-heading ld_fancy_heading_658527062806c">
-                    {" "}
-                    <h2 className="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left text-decoration-default">
-                      {" "}
-                      Our Technologies
-                    </h2>
-                  </div>
-                  <div className="ld-fancy-heading ld_fancy_heading_658527062c148">
-                    {" "}
-                    <p className="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left text-decoration-default">
-                      {" "}
-                      We unleash your business potential by maximising the
-                      innovation.
-                    </p>
-                  </div>
-                </div>
-              </div>
+      <div className="mt-5 text-center">
+        <div className="wpb_wrapper">
+          <div className="ld-fancy-heading ld_fancy_heading_658527062806c">
+            {" "}
+            <h2 className="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left text-decoration-default">
+              {" "}
+              Our Technologies
+            </h2>
+          </div>
+          <div className="ld-fancy-heading ld_fancy_heading_658527062c148">
+            {" "}
+            <p className="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left text-decoration-default">
+              {" "}
+              We unleash your business potential by maximising the
+              innovation.
+            </p>
+          </div>
+        </div>
+      </div>
 
 
-         <div class="col-12">
-                <ul class="partnerslist mt-5">
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-react-100.png"
-                      data-src="https://srvinfotech.com/public/img/partners/g-cloud.png"
-                      alt="Google Cloud"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-kotlin-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/duns.png"
-                      alt="DUNS"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-flutter-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/clutch.png"
-                      alt="Clutch"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-nodejs-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/nasscom.svg"
-                      alt="NASSCOM"
-                      data-loaded="true"
-                    />
-                  </li>
 
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-python-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/duns.png"
-                      alt="DUNS"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-c++-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/clutch.png"
-                      alt="Clutch"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-swift-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/nasscom.svg"
-                      alt="NASSCOM"
-                      data-loaded="true"
-                    />
-                  </li>
 
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-html-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/duns.png"
-                      alt="DUNS"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-javascript-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/clutch.png"
-                      alt="Clutch"
-                      data-loaded="true"
-                    />
-                  </li>
-                  <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-css-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/nasscom.svg"
-                      alt="NASSCOM"
-                      data-loaded="true"
-                    />
-                  </li>
+      <Marquee pauseOnHover={true} style={{ marginTop: '3em' }}>
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-css-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/nasscom.svg"
+          alt="NASSCOM"
+          data-loaded="true"
+        />
 
-                  {/* <li>
-                    <img
-                      class="loadimg"
-                      src="/img/development/icons8-angular-48.png"
-                      data-src="https://srvinfotech.com/public/img/partners/g-cloud.png"
-                      alt="Google Cloud"
-                      data-loaded="true"
-                    />
-                  </li> */}
-                </ul>
-              </div>
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-javascript-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/clutch.png"
+          alt="Clutch"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px', width: '80px', height: "auto" }}
+          src="/img/development/firebase.png"
+          data-src="https://srvinfotech.com/public/img/partners/clutch.png"
+          alt="Clutch"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-html-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/duns.png"
+          alt="DUNS"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-c++-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/clutch.png"
+          alt="Clutch"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-python-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/duns.png"
+          alt="DUNS"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-flutter-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/clutch.png"
+          alt="Clutch"
+          data-loaded="true"
+        />
+
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-nodejs-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/nasscom.svg"
+          alt="NASSCOM"
+          data-loaded="true"
+        />
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-kotlin-48.png"
+          data-src="https://srvinfotech.com/public/img/partners/duns.png"
+          alt="DUNS"
+          data-loaded="true"
+        />
+
+        <img
+          class="loadimg"
+          style={{ marginRight: '100px' }}
+          src="/img/development/icons8-react-100.png"
+          data-src="https://srvinfotech.com/public/img/partners/g-cloud.png"
+          alt="Google Cloud"
+          data-loaded="true"
+        />
+
+      </Marquee>
     </div>
   )
 }
 
-export default Slider
+export default Slider2
